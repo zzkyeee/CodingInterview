9b86ebe0747e289ef9981888db399e57911dfd0b
zhangzhikai <zhangzhikai@corp.netease.com>
2019-04-16 23:46:16 +0800

    Complete the comments and readme for Ex05

--- src/com/zzk/Ex05.java
+++ src/com/zzk/Ex05.java
@@ -14,10 +14,13 @@ public class Ex05 {
 	 * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
 	 */
 	public static String replaceSpace(StringBuffer str) {
+		/**
+		 * 先获取空格数扩充字符串，然后考虑通过从后往前遍历，时间复杂度为o(n)，若从前往后则需要o(n^2)
+		 */
 		if (str.length() == 0) {
 			return "";
 		}
-		// 空格数目
+		// 字符串中的空格数目
 		int spaceNum = 0;
 		// 1.第一次遍历，获取空格数目
 		int length = str.length();
@@ -28,14 +31,14 @@ public class Ex05 {
 				++spaceNum;
 			}
 		}
+		// 2.扩充长度，因为要填充%20，减去已存在的一个空格长度，则每个空格数需要多两个空格位
 		int newLength = str.length() + spaceNum * 2;
-		// 扩充长度
 		str.setLength(newLength);
-		// 定义两个"指针"
 		int p1 = length - 1;
 		int p2 = newLength - 1;
 		while (p1 != p2) {
 			char begin = str.charAt(p1);
+			// 3.1 若p1对应的字符为空格，则p2所对应的下标开始填充%20
 			if (begin == space) {
 				// 填充%20
 				str.setCharAt(p2--, '0');
@@ -43,7 +46,7 @@ public class Ex05 {
 				str.setCharAt(p2--, '%');
 				p1--;
 			} else {
-				// 复制
+				// 3.2 不是空格则直接复制
 				str.setCharAt(p1--, space);
 				str.setCharAt(p2--, begin);
 			}
